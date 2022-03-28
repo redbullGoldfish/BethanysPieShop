09154e763368c5cd4258eb8f3d973110578a5844
Rick Ramos <Ricardo_Ramos@txs.uscourts.gov>
2022-03-27 20:04:24 -0500

    integrated api

--- BethanysPieShopHRM.App/Services/EmployeeDataService.cs
+++ BethanysPieShopHRM.App/Services/EmployeeDataService.cs
@@ -2,12 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace BethanysPieShopHRM.App.Services
 {
     public class EmployeeDataService : IEmployeeDataService
     {
+        private readonly HttpClient _httpClient;
+
+        public EmployeeDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
         public Task<Employee> AddEmployee(Employee employee)
         {
             throw new NotImplementedException();
@@ -18,14 +27,16 @@ namespace BethanysPieShopHRM.App.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Employee>> GetAllEmployees()
+        public async Task<IEnumerable<Employee>> GetAllEmployees()
         {
-            throw new NotImplementedException();
+            return await JsonSerializer.DeserializeAsync<IEnumerable<Employee>>
+                (await _httpClient.GetStreamAsync($"api/employee"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
-        public Task<Employee> GetEmployeeDetails(int employeeId)
+        public async Task<Employee> GetEmployeeDetails(int employeeId)
         {
-            throw new NotImplementedException();
+            return await JsonSerializer.DeserializeAsync<Employee>
+                (await _httpClient.GetStreamAsync($"api/employee/{employeeId}"), new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
         public Task UpdateEmployee(Employee employee)
