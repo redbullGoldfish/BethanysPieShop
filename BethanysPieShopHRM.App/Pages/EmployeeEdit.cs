6c543c4649962e475c4a1ddeeaa349b043c67a58
Rick Ramos <Ricardo_Ramos@txs.uscourts.gov>
2022-03-28 11:50:31 -0500

    added country and job category dropdown options

--- BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.cs
@@ -12,15 +12,29 @@ namespace BethanysPieShopHRM.App.Pages
     {
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
+        [Inject]
+        public ICountryDataService CountryDataService { get; set; }
+        [Inject]
+        public IJobCategoryDataService JobCategoryDataService { get; set; }
 
         [Parameter]
         public string EmployeeId { get; set; }
 
         public Employee Employee { get; set; } = new Employee();
+        public List<Country> Countries { get; set; } = new List<Country>();
+        public List<JobCategory> JobCategories { get; set; } = new List<JobCategory>();
+
+        protected string CountryId = string.Empty;
+        protected string JobCategoryId = string.Empty;
 
         protected override async Task OnInitializedAsync()
         {
+            Countries = (await CountryDataService.GetAllCountries()).ToList();
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+            JobCategories = (await JobCategoryDataService.GetAllJobCategories()).ToList();
+
+            CountryId = Employee.CountryId.ToString();
+            JobCategoryId = Employee.JobCategoryId.ToString();
         }
     }
 }
