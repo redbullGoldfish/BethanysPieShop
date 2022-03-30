62c8e32da787f4d85f4ba0d34d80f9c75b091918
Rick Ramos <Ricardo_Ramos@txs.uscourts.gov>
2022-03-29 22:16:33 -0500

    added quick add component for employees page

--- BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.cs
@@ -1,4 +1,5 @@
-﻿using BethanysPieShopHRM.App.Services;
+﻿using BethanysPieShopHRM.App.Components;
+using BethanysPieShopHRM.App.Services;
 using BethanysPieShopHRM.Shared;
 using Microsoft.AspNetCore.Components;
 using System;
@@ -10,14 +11,27 @@ namespace BethanysPieShopHRM.App.Pages
 {
     public partial class EmployeeOverview
     {
+        public IEnumerable<Employee> Employees { get; set; }
+
         [Inject]
         public IEmployeeDataService EmployeeDataService { get; set; }
 
-        public IEnumerable<Employee> Employees { get; set; }
+        protected AddEmployeeDialog AddEmployeeDialog { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
             Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+        }
+
+        protected void QuickAddEmployee()
+        {
+            AddEmployeeDialog.Show();
+        }
+
+        public async void AddEmployeeDialog_OnDialogClose()
+        {
+            Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
+            StateHasChanged();
         }
     }
 }
